96277046342a7047f75a19d64dc0268dd0594ee0
CloudTechDevOps <cloud87777@gmail.com>
2024-02-12 07:55:43 +0530

    added python script

---
+++ python.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    client = boto3.client('ec2', region_name= 'us-east-1')
+    response = client.run_instances(
+           ImageId='ami-0e9107ed11be76fde',
+           InstanceType='t2.micro',
+           KeyName='docker',
+           MaxCount=1,
+           MinCount=1
+           )
